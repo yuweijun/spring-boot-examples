ac50b47ada20cf8ce6ecbeae4c8f3cbaa4ae60eb
yuweijun <yuweijun@live.com>
2017-12-12 15:52:40 +0800

    add ExampleInfoContributor

---
+++ spring-boot-jsp/src/main/java/com/example/spring/jsp/ExampleInfoContributor.java
@@ -0,0 +1,20 @@
+package com.example.spring.jsp;
+
+import org.springframework.boot.actuate.info.Info;
+import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+
+/**
+ * Created by yuweijun on 2017-12-12.
+ */
+@Component
+public class ExampleInfoContributor implements InfoContributor {
+
+    @Override
+    public void contribute(Info.Builder builder) {
+        builder.withDetail("example", Collections.singletonMap("key", "value"));
+    }
+
+}
