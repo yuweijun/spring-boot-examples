db00d6b32fab7f576a7099d6602ce2fb40a1dc70
yuweijun <yuweijun@live.com>
2017-12-19 17:48:53 +0800

    get java process pid

---
+++ spring-boot-jsp/src/test/java/com/example/spring/jsp/GetPidOfJavaApplication.java
@@ -0,0 +1,23 @@
+package com.example.spring.jsp;
+
+import org.junit.Test;
+
+import java.lang.management.ManagementFactory;
+
+/**
+ * Created by yuweijun on 2017-12-19.
+ */
+public class GetPidOfJavaApplication {
+
+    @Test
+    public void pid() {
+        try {
+            String jvmName = ManagementFactory.getRuntimeMXBean().getName();
+            System.out.println(jvmName);
+            System.out.println(jvmName.split("@")[0]);
+        } catch (Throwable ex) {
+            ex.printStackTrace();
+        }
+    }
+
+}
--- spring-boot-jsp/src/test/java/com/example/spring/jsp/SpringBootTestRandomPortTest.java
+++ spring-boot-jsp/src/test/java/com/example/spring/jsp/SpringBootTestRandomPortTest.java
@@ -13,7 +13,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class SpringBottTestRandomPortTest {
+public class SpringBootTestRandomPortTest {
 
     @Autowired
     EmbeddedWebApplicationContext server;
