aead202c92b0f0c091d5884f9651dd6903a8121d
yuweijun <yuweijun@live.com>
2018-05-23 22:17:49 +0800

    add uri encode decode test

--- spring-boot-jsp/src/test/java/com/example/spring/boot/UriUtilsTest.java
+++ spring-boot-jsp/src/test/java/com/example/spring/boot/UriUtilsTest.java
@@ -4,35 +4,43 @@ import org.junit.Test;
 import org.springframework.web.util.UriUtils;
 
 import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 
 /**
  * Utility class for URI encoding and decoding based on RFC 3986. Offers encoding methods for the various URI components.
- *
- * Created by yuweijun on 2018-04-20.
  */
 public class UriUtilsTest {
 
     @Test
     public void testCase1() throws UnsupportedEncodingException {
-        String encode = UriUtils.encode("Hello World-_.~", "UTF-8");
-        System.out.println(encode);
-    }
-    
-    @Test
-    public void testCase2() throws UnsupportedEncodingException {
-        String decode = UriUtils.decode("Hello%20World-_.~", "UTF-8");
-        System.out.println(decode);
+        String encode1 = UriUtils.encode("+|`^!'()*~-_.09azAZ", "UTF-8");
+        System.out.println(encode1);
+
+        String decode1 = UriUtils.decode("+|`^!'()*~-_.09azAZ", "UTF-8");
+        System.out.println(decode1);
+
+        String encode2 = URLEncoder.encode("+|`^!'()*~-_.09azAZ", "UTF-8");
+        System.out.println(encode2);
+
+        String decode2 = URLDecoder.decode("%2B%7C%60%5E%21%27%28%29*%7E-_.09azAZ", "UTF-8");
+        System.out.println(decode2);
     }
 
     @Test
     public void testCase3() throws UnsupportedEncodingException {
-        String encode = UriUtils.encode("Hello+World-_.~", "UTF-8");
-        System.out.println(encode);
+        String encode1 = UriUtils.encode(" |`^!'()*~-_.09azAZ", "UTF-8");
+        String encode2 = URLEncoder.encode(" |`^!'()*~-_.09azAZ", "UTF-8");
+        System.out.println(encode1);
+        System.out.println(encode2);
+
+        String decode1 = UriUtils.decode("%20%7C%60%5E%21%27%28%29*%7E-_.09azAZ", "UTF-8");
+        String decode2 = URLDecoder.decode("+%7C%60%5E%21%27%28%29*%7E-_.09azAZ", "UTF-8");
+        System.out.println(decode1);
+        System.out.println(decode2);
     }
 
     @Test
-    public void testCase4() throws UnsupportedEncodingException {
-        String decode = UriUtils.decode("Hello+World-_.~", "UTF-8");
-        System.out.println(decode);
+    public void testCase5() throws UnsupportedEncodingException {
     }
 }
