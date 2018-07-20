24777827061355a5e323f568daba2ede621916d3
yuweijun <yuweijun@live.com>
2018-07-20 12:40:17 +0800

    添加GsonHttpMessageConverter

--- spring-boot-mvc/src/main/java/com/example/spring/boot/WebMvcConfig.java
+++ spring-boot-mvc/src/main/java/com/example/spring/boot/WebMvcConfig.java
@@ -2,13 +2,30 @@ package com.example.spring.boot;
 
 import com.example.spring.boot.interceptor.SessionInterceptor;
 
+import com.google.gson.GsonBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.GsonHttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import java.util.List;
+
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        Jackson2ObjectMapperBuilder builder = Jackson2ObjectMapperBuilder.xml();
+        builder.indentOutput(true);
+        converters.add(new MappingJackson2XmlHttpMessageConverter(builder.build()));
+        GsonHttpMessageConverter gsonHttpMessageConverter = new GsonHttpMessageConverter();
+        gsonHttpMessageConverter.setGson(new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").create());
+        converters.add(gsonHttpMessageConverter);
+    }
 
     @Bean
     public SessionInterceptor sessionInterceptor() {
